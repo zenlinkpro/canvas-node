9791f2af342314205168b0d96663d3f9272ad13a
icodezjb <icodezjb@163.com>
2020-10-24 16:46:49 +0800

    Cargo fmt

--- zenlink/dex/src/lib.rs
+++ zenlink/dex/src/lib.rs
@@ -1,10 +1,10 @@
 // Ensure we're `no_std` when compiling for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{Parameter, decl_module, decl_event, decl_storage, decl_error, ensure};
-use sp_runtime::traits::{Member, AtLeast32Bit, AtLeast32BitUnsigned, Zero, StaticLookup};
+use frame_support::{decl_error, decl_event, decl_module, decl_storage, ensure, Parameter};
 use frame_system::{ensure_signed, RawOrigin};
 use sp_runtime::traits::One;
+use sp_runtime::traits::{AtLeast32Bit, AtLeast32BitUnsigned, Member, StaticLookup, Zero};
 
 /// The module configuration trait.
 pub trait Trait: frame_system::Trait {
@@ -19,64 +19,64 @@ pub trait Trait: frame_system::Trait {
 }
 
 decl_module! {
-	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		type Error = Error<T>;
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        type Error = Error<T>;
 
-		fn deposit_event() = default;
-		/// Issue a new class of fungible assets. There are, and will only ever be, `total`
-		/// such assets and they'll all belong to the `origin` initially. It will have an
-		/// identifier `AssetId` instance: this will be specified in the `Issued` event.
-		///
-		/// # <weight>
-		/// - `O(1)`
-		/// - 1 storage mutation (codec `O(1)`).
-		/// - 2 storage writes (condec `O(1)`).
-		/// - 1 event.
-		/// # </weight>
-		#[weight = 0]
-		fn issue(origin, #[compact] total: T::Balance) {
-			let origin = ensure_signed(origin)?;
+        fn deposit_event() = default;
+        /// Issue a new class of fungible assets. There are, and will only ever be, `total`
+        /// such assets and they'll all belong to the `origin` initially. It will have an
+        /// identifier `AssetId` instance: this will be specified in the `Issued` event.
+        ///
+        /// # <weight>
+        /// - `O(1)`
+        /// - 1 storage mutation (codec `O(1)`).
+        /// - 2 storage writes (condec `O(1)`).
+        /// - 1 event.
+        /// # </weight>
+        #[weight = 0]
+        fn issue(origin, #[compact] total: T::Balance) {
+            let origin = ensure_signed(origin)?;
 
-			let id = Self::next_asset_id();
-			<NextAssetId<T>>::mutate(|id| *id += One::one());
+            let id = Self::next_asset_id();
+            <NextAssetId<T>>::mutate(|id| *id += One::one());
 
-			<Balances<T>>::insert((id, &origin), total);
-			<TotalSupply<T>>::insert(id, total);
+            <Balances<T>>::insert((id, &origin), total);
+            <TotalSupply<T>>::insert(id, total);
 
-			Self::deposit_event(RawEvent::Issued(id, origin, total));
-		}
+            Self::deposit_event(RawEvent::Issued(id, origin, total));
+        }
 
-		/// Move some assets from one holder to another.
-		///
-		/// # <weight>
-		/// - `O(1)`
-		/// - 1 static lookup
-		/// - 2 storage mutations (codec `O(1)`).
-		/// - 1 event.
-		/// # </weight>
-		#[weight = 0]
-		fn transfer(origin,
-			#[compact] id: T::AssetId,
-			target: <T::Lookup as StaticLookup>::Source,
-			#[compact] amount: T::Balance
-		) {
-			let origin = ensure_signed(origin)?;
-			let origin_account = (id, origin.clone());
-			let origin_balance = <Balances<T>>::get(&origin_account);
-			let target = T::Lookup::lookup(target)?;
-			ensure!(!amount.is_zero(), Error::<T>::AmountZero);
-			ensure!(origin_balance >= amount, Error::<T>::BalanceLow);
+        /// Move some assets from one holder to another.
+        ///
+        /// # <weight>
+        /// - `O(1)`
+        /// - 1 static lookup
+        /// - 2 storage mutations (codec `O(1)`).
+        /// - 1 event.
+        /// # </weight>
+        #[weight = 0]
+        fn transfer(origin,
+            #[compact] id: T::AssetId,
+            target: <T::Lookup as StaticLookup>::Source,
+            #[compact] amount: T::Balance
+        ) {
+            let origin = ensure_signed(origin)?;
+            let origin_account = (id, origin.clone());
+            let origin_balance = <Balances<T>>::get(&origin_account);
+            let target = T::Lookup::lookup(target)?;
+            ensure!(!amount.is_zero(), Error::<T>::AmountZero);
+            ensure!(origin_balance >= amount, Error::<T>::BalanceLow);
 
-			Self::deposit_event(RawEvent::Transferred(id, origin, target.clone(), amount));
-			<Balances<T>>::insert(origin_account, origin_balance - amount);
-			<Balances<T>>::mutate((id, target), |balance| *balance += amount);
-		}
+            Self::deposit_event(RawEvent::Transferred(id, origin, target.clone(), amount));
+            <Balances<T>>::insert(origin_account, origin_balance - amount);
+            <Balances<T>>::mutate((id, target), |balance| *balance += amount);
+        }
 
         #[weight = 0]
         fn allow(origin,
             #[compact] id: T::AssetId,
             spender: <T::Lookup as StaticLookup>::Source,
-			#[compact] amount: T::Balance
+            #[compact] amount: T::Balance
         ) {
             let owner = ensure_signed(origin)?;
             let spender = T::Lookup::lookup(spender)?;
@@ -91,8 +91,8 @@ decl_module! {
             #[compact] id: T::AssetId,
             from: <T::Lookup as StaticLookup>::Source,
             target: <T::Lookup as StaticLookup>::Source,
-			#[compact] amount: T::Balance
-		){
+            #[compact] amount: T::Balance
+        ){
             let spender = ensure_signed(origin.clone())?;
             let owner = T::Lookup::lookup(from)?;
 
@@ -102,51 +102,51 @@ decl_module! {
             <Allowances<T>>::insert((id, owner.clone(), spender), allowance - amount);
 
             Self::transfer(<T as frame_system::Trait>::Origin::from(RawOrigin::Signed(owner)), id, target, amount)?;
-		}
-	}
+        }
+    }
 }
 
 decl_event! {
-	pub enum Event<T> where
-		<T as frame_system::Trait>::AccountId,
-		<T as Trait>::Balance,
-		<T as Trait>::AssetId,
-	{
-		/// Some assets were issued. \[asset_id, owner, total_supply\]
-		Issued(AssetId, AccountId, Balance),
-		/// Some assets were transferred. \[asset_id, from, to, amount\]
-		Transferred(AssetId, AccountId, AccountId, Balance),
-		/// Some assets were allowable \[asset_id, owner, spender, amount\]
-		Approval(AssetId, AccountId, AccountId, Balance),
-	}
+    pub enum Event<T> where
+        <T as frame_system::Trait>::AccountId,
+        <T as Trait>::Balance,
+        <T as Trait>::AssetId,
+    {
+        /// Some assets were issued. \[asset_id, owner, total_supply\]
+        Issued(AssetId, AccountId, Balance),
+        /// Some assets were transferred. \[asset_id, from, to, amount\]
+        Transferred(AssetId, AccountId, AccountId, Balance),
+        /// Some assets were allowable \[asset_id, owner, spender, amount\]
+        Approval(AssetId, AccountId, AccountId, Balance),
+    }
 }
 
 decl_error! {
-	pub enum Error for Module<T: Trait> {
-		/// Transfer amount should be non-zero
-		AmountZero,
-		/// Account balance must be greater than or equal to the transfer amount
-		BalanceLow,
-		/// Balance should be non-zero
-		BalanceZero,
-		/// Account allowance balance must be greater than or equal to the transfer_from amount
-		AllowanceLow,
-	}
+    pub enum Error for Module<T: Trait> {
+        /// Transfer amount should be non-zero
+        AmountZero,
+        /// Account balance must be greater than or equal to the transfer amount
+        BalanceLow,
+        /// Balance should be non-zero
+        BalanceZero,
+        /// Account allowance balance must be greater than or equal to the transfer_from amount
+        AllowanceLow,
+    }
 }
 
 decl_storage! {
-	trait Store for Module<T: Trait> as Assets {
-		/// The number of units of assets held by any given account.
-		Balances: map hasher(blake2_128_concat) (T::AssetId, T::AccountId) => T::Balance;
-		/// The next asset identifier up for grabs.
-		NextAssetId get(fn next_asset_id): T::AssetId;
-		/// The total unit supply of an asset.
-		///
-		/// TWOX-NOTE: `AssetId` is trusted, so this is safe.
-		TotalSupply: map hasher(twox_64_concat) T::AssetId => T::Balance;
+    trait Store for Module<T: Trait> as Assets {
+        /// The number of units of assets held by any given account.
+        Balances: map hasher(blake2_128_concat) (T::AssetId, T::AccountId) => T::Balance;
+        /// The next asset identifier up for grabs.
+        NextAssetId get(fn next_asset_id): T::AssetId;
+        /// The total unit supply of an asset.
+        ///
+        /// TWOX-NOTE: `AssetId` is trusted, so this is safe.
+        TotalSupply: map hasher(twox_64_concat) T::AssetId => T::Balance;
         /// The allowance of assets held by spender who can spend from owner
-		Allowances: map hasher(blake2_128_concat) (T::AssetId, T::AccountId, T::AccountId) => T::Balance;
-	}
+        Allowances: map hasher(blake2_128_concat) (T::AssetId, T::AccountId, T::AccountId) => T::Balance;
+    }
 }
 
 // The main implementation block for the module.
@@ -173,22 +173,28 @@ impl<T: Trait> Module<T> {
 mod tests {
     use super::*;
 
-    use frame_support::{impl_outer_origin, assert_ok, assert_noop, parameter_types, weights::Weight};
+    use frame_support::{
+        assert_noop, assert_ok, impl_outer_origin, parameter_types, weights::Weight,
+    };
     use sp_core::H256;
-    use sp_runtime::{Perbill, traits::{BlakeTwo256, IdentityLookup}, testing::Header};
+    use sp_runtime::{
+        testing::Header,
+        traits::{BlakeTwo256, IdentityLookup},
+        Perbill,
+    };
 
     impl_outer_origin! {
-		pub enum Origin for Test where system = frame_system {}
-	}
+        pub enum Origin for Test where system = frame_system {}
+    }
 
     #[derive(Clone, Eq, PartialEq)]
     pub struct Test;
     parameter_types! {
-		pub const BlockHashCount: u64 = 250;
-		pub const MaximumBlockWeight: Weight = 1024;
-		pub const MaximumBlockLength: u32 = 2 * 1024;
-		pub const AvailableBlockRatio: Perbill = Perbill::one();
-	}
+        pub const BlockHashCount: u64 = 250;
+        pub const MaximumBlockWeight: Weight = 1024;
+        pub const MaximumBlockLength: u32 = 2 * 1024;
+        pub const AvailableBlockRatio: Perbill = Perbill::one();
+    }
     impl frame_system::Trait for Test {
         type BaseCallFilter = ();
         type Origin = Origin;
@@ -224,7 +230,10 @@ mod tests {
     type Assets = Module<Test>;
 
     fn new_test_ext() -> sp_io::TestExternalities {
-        frame_system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+        frame_system::GenesisConfig::default()
+            .build_storage::<Test>()
+            .unwrap()
+            .into()
     }
 
     #[test]
@@ -267,7 +276,10 @@ mod tests {
         new_test_ext().execute_with(|| {
             assert_ok!(Assets::issue(Origin::signed(1), 100));
             assert_eq!(Assets::balance(0, 1), 100);
-            assert_noop!(Assets::transfer(Origin::signed(1), 0, 2, 0), Error::<Test>::AmountZero);
+            assert_noop!(
+                Assets::transfer(Origin::signed(1), 0, 2, 0),
+                Error::<Test>::AmountZero
+            );
         });
     }
 
@@ -276,7 +288,10 @@ mod tests {
         new_test_ext().execute_with(|| {
             assert_ok!(Assets::issue(Origin::signed(1), 100));
             assert_eq!(Assets::balance(0, 1), 100);
-            assert_noop!(Assets::transfer(Origin::signed(1), 0, 2, 101), Error::<Test>::BalanceLow);
+            assert_noop!(
+                Assets::transfer(Origin::signed(1), 0, 2, 101),
+                Error::<Test>::BalanceLow
+            );
         });
     }
 
@@ -323,7 +338,10 @@ mod tests {
             assert_ok!(Assets::allow(Origin::signed(1), 0, 2, 20));
             assert_eq!(Assets::allowances(0, 1, 2), 20);
 
-            assert_noop!(Assets::transfer_from(Origin::signed(2), 0, 1, 3, 100), Error::<Test>::AllowanceLow);
+            assert_noop!(
+                Assets::transfer_from(Origin::signed(2), 0, 1, 3, 100),
+                Error::<Test>::AllowanceLow
+            );
         });
     }
 }
